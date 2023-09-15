b3a71e166db79c7bbc954e0a256e2ef46e768c3b
KatsutoshiOtogawa <gh1ab7im@gmail.com>
2023-09-16 05:45:32 +0900

    update

---
+++ which.ts
@@ -0,0 +1,147 @@
+
+import { globToRegExp, extname,join } from 'path/mod.ts';
+import { walk, exists, existsSync } from 'fs/mod.ts';
+
+// const pathExtText = environment.env("PATHEXT") ?? ".EXE;.CMD;.BAT;.COM";
+  
+/** runtime 環境下でwindowsかどうかの確認に使う。 */
+const isWindows = Deno.build.os === "windows";
+
+/**
+ * 
+ * @returns {":" | ";"} runtime環境下のセパレーターを返します。
+ */
+function envSeparator (): ":" | ";" {
+    if (isWindows) {
+        return ";";
+    }
+
+    return ":";
+}
+
+class NotSupportedPlatform extends Error {
+    
+}
+
+class NotSupportedVersion extends Error {
+
+}
+
+// Deno.version.deno
+
+// support
+const support_version = [
+    "1.36.3", 
+    "1.36.4"
+];
+
+/** コマンドが一つでもなかったら作成する */
+// class  extends Error {
+    
+// }
+
+function is_support_version (_deno_version: string): boolean {
+
+    return true;
+
+    // 今は動いているが今後動作が変わったら、下を使う。
+    // return support_version.includes(deno_version);
+}
+
+interface Opts {
+    all?: boolean,
+    read_alias?: boolean
+};
+
+
+// 後で実装
+function isExecutable ():boolean {
+
+    // call faccessat 
+
+    return true;
+}
+
+/**
+ * 
+ * @param  {[string, ...string[]]} commands you want to search command. command multi
+ * @param {[Opts]} opts command option
+ * @returns {Promise<{path: string[], err?: Error}>}
+ * 
+ */
+function which (commands: [string, ...string[]], opts?: Opts): {path: string[], err?: Error} {
+
+    let _all = false;
+    let _read_alias = false;
+    if (opts) {
+        const {all, read_alias} = opts;
+        if (all !== undefined) {
+            _all = all;
+        }
+        if (read_alias !== undefined) {
+            _read_alias = read_alias;
+        }
+    }
+
+    let path: string[] = [];
+    let err: Error | undefined = undefined;
+
+    // パスが読み取り可能かどうかも入れる。
+    const search_path_env = Deno.env.get("PATH");
+
+    if (search_path_env === undefined) {
+        err = new Error("Not Read PATH Environment Variable");
+        return {path, err}
+    }
+
+    // 
+    // 有効なパス一覧
+    const validated_paths = search_path_env.split(envSeparator()).filter(v => existsSync(v));
+
+    // コマンド単位で検索
+    for (const command of commands) {
+        // 実行可能かのチェック
+        const command_paths = validated_paths
+            .map(v => join(v, command));
+
+        let validated: string[] = [];
+        // 前検索
+        if (_all) {
+            validated = command_paths
+                .filter(v => existsSync(v) && isExecutable());
+        } else {
+            const finded = command_paths
+                .find(v => existsSync(v) && isExecutable());
+            if (finded) {
+                validated = [finded];
+            }
+        }
+
+        if (validated.length === 0) {
+
+            err = new Error(`${command} is not found`);
+        } else {
+            path = [...path, ...validated];
+        }
+    }
+
+    // コマンドごとに検索をかけるのasyncループ
+
+    // コマンドごとに検索
+
+    // インデックスが最初のパスから検索する
+    // パスの存在チェックを前に入れる。
+
+    return {path, err}
+// 存在しない場合はerrorにしたいなら引数に渡す。
+    // if (isWindows) {
+    //     console.log("windows")
+    // } else {
+
+    // }
+    // return "";
+}
+
+export {
+    which 
+}
---
+++ which_test.ts
@@ -0,0 +1,70 @@
+import { assertEquals,  assertIsError  } from "https://deno.land/std@0.201.0/assert/mod.ts";
+import { which } from './which.ts';
+
+// which は一つのコマンドを複数の引数を持てる
+Deno.test(function singleTest() {
+  
+  const result = which(["cat"]);
+  assertEquals(result.path[0], "/usr/bin/cat");
+  // 成功したのでundefined
+  assertEquals(result.err, undefined);
+});
+
+// 存在しないコマンドを与えるとエラーを引数として返す
+Deno.test(function singleNotFoundTest() {
+  
+  const result = which(["Notfound"]);
+
+  assertEquals(result.path.length, 0);
+  assertIsError(result.err)
+});
+
+// which は複数のコマンドを複数の引数を持てる
+Deno.test(function multiTest() {
+  
+  const result = which(["cat", "ls", "env"]);
+  assertEquals(result.path[0], "/usr/bin/cat");
+  assertEquals(result.path[1], "/usr/bin/ls");
+  assertEquals(result.path[2], "/usr/bin/env");
+  assertEquals(result.path.length, 3);
+
+  // 成功したのでundefined
+  assertEquals(result.err, undefined);
+});
+
+// 一つでも見つからないコマンドを発見したら、エラー
+Deno.test(function multiNotFoundTest() {
+  
+  const result = which(["cat", "NotFound"]);
+  assertEquals(result.path[0], "/usr/bin/cat");
+  assertEquals(result.path.length, 1);
+  assertIsError(result.err);
+
+  const result2 = which(["cat", "ls", "NotFound"]);
+  assertEquals(result2.path[0], "/usr/bin/cat");
+  assertEquals(result2.path[1], "/usr/bin/ls");
+  assertEquals(result2.path.length, 2);
+  assertIsError(result2.err);
+});
+
+// setfacl -R -m u:vscode:r
+
+// whichファイルの存在確認と実行権限確認。
+// if [ -f "$ELEMENT/$PROGRAM" ] && [ -x "$ELEMENT/$PROGRAM" ]; then
+//      puts "$ELEMENT/$PROGRAM"
+//      RET=0
+//      [ "$ALLMATCHES" -eq 1 ] || break
+//     fi
+
+
+// which の仕様としてPATHに存在しなパスが合ってもエラーにならない。
+
+// そのユーザーに対してacl含むファイルのパーミッション的に実行可能出ないと表示しない。
+// 例えば、rootユーザーのみに実行権限が合って、通常ユーザーに実行権限がないならwhichに表示されない。
+
+// acl的に実行できるかも必要になる。
+
+// o+xなら実行可能,u+xならファイルの所有者を見れば良い
+
+// windowsの仕様に合わせず、which として動作する拡張子を勝手に補完しない
+// windowsの仕様に合わせるならwhereを使う。
